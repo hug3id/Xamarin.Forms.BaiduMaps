ab56b9a0b4ac779850c9a4753b8a3f0df00a82f3
hug3id <hug3id@gmail.com>
2016-08-31 15:42:37 +0800

    Refactor map events

---
+++ Xamarin.Forms.BaiduMaps/MapEvents.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Xamarin.Forms.BaiduMaps
+{
+    public class MapBlankClickedEventArgs : EventArgs
+    {
+        public Coordinate Coordinate { get; }
+        public MapBlankClickedEventArgs(Coordinate coordinate)
+        {
+            Coordinate = coordinate;
+        }
+    }
+
+    public class MapDoubleClickedEventArgs : EventArgs
+    {
+        public Coordinate Coordinate { get; }
+        public MapDoubleClickedEventArgs(Coordinate coordinate)
+        {
+            Coordinate = coordinate;
+        }
+    }
+
+    public class MapLongClickedEventArgs : EventArgs
+    {
+        public Coordinate Coordinate { get; }
+        public MapLongClickedEventArgs(Coordinate coordinate)
+        {
+            Coordinate = coordinate;
+        }
+    }
+
+    public class Poi
+    {
+        public Coordinate Coordinate { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class MapPoiClickedEventArgs : EventArgs
+    {
+        public Poi Poi { get; }
+        public MapPoiClickedEventArgs(Poi poi)
+        {
+            Poi = Poi;
+        }
+    }
+}
+
