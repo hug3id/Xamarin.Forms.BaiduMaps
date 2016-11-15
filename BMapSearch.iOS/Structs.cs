cf60cb9dc1c0266e2a9b7d20e99a20ab5c2ce32c
hug3id <hug3id@gmail.com>
2016-11-15 17:55:29 +0800

    Update to Baidu SDK v3.1

--- BMapSearch.iOS/Structs.cs
+++ BMapSearch.iOS/Structs.cs
@@ -5,190 +5,167 @@ using Foundation;
 
 namespace BMapSearch
 {
-	/*static class CFunctions
-	{
-		// extern NSString * BMKGetMapApiSearchComponentVersion () __attribute__((visibility("default")));
-		[DllImport ("__Internal")]
-		[Verify (PlatformInvoke)]
-		static extern NSString BMKGetMapApiSearchComponentVersion ();
+    //[Verify (InferredFromMemberPrefix)]
+    public enum BMK
+    {
+        InvalidCoordinate = -1,
+        CarTrafficFIRST = 60,
+        CarTimeFirst = 0,
+        CarDisFirst,
+        CarFeeFirst,
+        BusTimeFirst,
+        BusTransferFirst,
+        BusWalkFirst,
+        BusNoSubway,
+        TypeCityList = 7,
+        TypePoiList = 11,
+        TypeAreaPoiList = 21,
+        TypeAreaMultiPoiList = 45
+    }
 
-		// extern BOOL BMKCheckSearchComponentIsLegal () __attribute__((visibility("default")));
-		[DllImport ("__Internal")]
-		[Verify (PlatformInvoke)]
-		static extern bool BMKCheckSearchComponentIsLegal ();
-	}*/
+    /*public enum BMKErrorCode : uint
+    {
+        Ok = 0,
+        Connect = 2,
+        Data = 3,
+        RouteAddr = 4,
+        ResultNotFound = 100,
+        LocationFailed = 200,
+        PermissionCheckFailure = 300,
+        Parse = 310
+    }
 
-	//[Verify (InferredFromMemberPrefix)]
-	public enum BMK
-	{
-		InvalidCoordinate = -1,
-		CarTrafficFIRST = 60,
-		CarTimeFirst = 0,
-		CarDisFirst,
-		CarFeeFirst,
-		BusTimeFirst,
-		BusTransferFirst,
-		BusWalkFirst,
-		BusNoSubway,
-		TypeCityList = 7,
-		TypePoiList = 11,
-		TypeAreaPoiList = 21,
-		TypeAreaMultiPoiList = 45
-	}
+    public enum BMKPermissionCheckResultCode
+    {
+        ConnectError = -300,
+        DataError = -200,
+        Ok = 0,
+        KeyError = 101,
+        McodeError = 102,
+        UidKeyError = 200,
+        KeyForbiden = 201
+    }
 
-	/*public enum BMKErrorCode : uint
-	{
-		Ok = 0,
-		Connect = 2,
-		Data = 3,
-		RouteAddr = 4,
-		ResultNotFound = 100,
-		LocationFailed = 200,
-		PermissionCheckFailure = 300,
-		Parse = 310
-	}
+    public enum BMKSearchErrorCode : uint
+    {
+        NoError = 0,
+        AmbiguousKeyword,
+        AmbiguousRoureAddr,
+        NotSupportBus,
+        NotSupportBus2city,
+        ResultNotFound,
+        StEnTooNear,
+        KeyError,
+        NetwokrError,
+        NetwokrTimeout,
+        PermissionUnfinished,
+        IndoorIdError,
+        FloorError,
+        IndoorRouteNoInBuilding,
+        IndoorRouteNoInSameBuilding,
+        ParameterError
+    }
 
-	public enum BMKPermissionCheckResultCode
-	{
-		ConnectError = -300,
-		DataError = -200,
-		Ok = 0,
-		KeyError = 101,
-		McodeError = 102,
-		UidKeyError = 200,
-		KeyForbiden = 201
-	}
+    public enum BMKOpenErrorCode : uint
+    {
+        NoError = 0,
+        WebMap,
+        OptionNull,
+        NotSupport,
+        PoiDetailUidNull,
+        PoiNearbyKeywordNull,
+        RouteStartError,
+        RouteEndError,
+        PanoramaUidError,
+        PanoramaAbsent,
+        PermissionUnfinished,
+        KeyError,
+        NetwokrError
+    }*/
 
-	public enum BMKSearchErrorCode : uint
-	{
-		NoError = 0,
-		AmbiguousKeyword,
-		AmbiguousRoureAddr,
-		NotSupportBus,
-		NotSupportBus2city,
-		ResultNotFound,
-		StEnTooNear,
-		KeyError,
-		NetwokrError,
-		NetwokrTimeout,
-		PermissionUnfinished,
-		IndoorIdError,
-		FloorError
-	}
+    public enum BMKTransitStepType : uint
+    {
+        Busline = 0,
+        Subway = 1,
+        Wakling = 2
+    }
 
-	public enum BMKOpenErrorCode : uint
-	{
-		NoError = 0,
-		WebMap,
-		OptionNull,
-		NotSupport,
-		PoiDetailUidNull,
-		PoiNearbyKeywordNull,
-		RouteStartError,
-		RouteEndError,
-		PanoramaUidError,
-		PanoramaAbsent,
-		PermissionUnfinished,
-		KeyError,
-		NetwokrError
-	}
+    public enum BMKMassTransitType : uint
+    {
+        Subway = 0,
+        Train = 1,
+        Plane = 2,
+        Busline = 3,
+        Driving = 4,
+        Wakling = 5,
+        Coach = 6
+    }
 
-	[StructLayout (LayoutKind.Sequential)]
-	public struct BMKCoordinateSpan
-	{
-		public double latitudeDelta;
+    public enum BMKIndoorStepNodeType : uint
+    {
+        Elevator = 1,
+        Escalator = 2,
+        Stair = 3,
+        SecurityCheck = 4
+    }
 
-		public double longitudeDelta;
-	}
+    public enum BMKTransitPolicy : uint
+    {
+        TimeFirst = 3,
+        TransferFirst = 4,
+        WalkFirst = 5,
+        NoSubway = 6
+    }
 
-	[StructLayout (LayoutKind.Sequential)]
-	public struct BMKCoordinateBounds
-	{
-		public CLLocationCoordinate2D northEast;
+    public enum BMKMassTransitIncityPolicy : uint
+    {
+        Recommend = 0,
+        TransferFirst = 1,
+        WalkFirst = 2,
+        NoSubway = 3,
+        TimeFirst = 4,
+        SubwayFirst = 5
+    }
 
-		public CLLocationCoordinate2D southWest;
-	}
+    public enum BMKMassTransitIntercityPolicy : uint
+    {
+        TimeFirst = 0,
+        StartEarly = 1,
+        PriceFirst = 2
+    }
 
-	[StructLayout (LayoutKind.Sequential)]
-	public struct BMKCoordinateRegion
-	{
-		public CLLocationCoordinate2D center;
+    public enum BMKMassTransitIntercityTransPolicy : uint
+    {
+        TrainFirst = 0,
+        PlaneFirst = 1,
+        BusFirst = 2
+    }
 
-		public BMKCoordinateSpan span;
-	}
+    public enum BMKDrivingPolicy
+    {
+        BlkFirst = -1,
+        TimeFirst = 0,
+        DisFirst = 1,
+        FeeFirst
+    }
 
-	[StructLayout (LayoutKind.Sequential)]
-	public struct BMKGeoPoint
-	{
-		public int latitudeE6;
+    public enum BMKDrivingRequestTrafficType : uint
+    {
+        None = 0,
+        PathAndTraffice = 1
+    }
 
-		public int longitudeE6;
-	}
+    public enum BMKPoiSortType : uint
+    {
+        Composite = 0,
+        Distance
+    }
 
-	[StructLayout (LayoutKind.Sequential)]
-	public struct BMKMapPoint
-	{
-		public double x;
-
-		public double y;
-	}
-
-	[StructLayout (LayoutKind.Sequential)]
-	public struct BMKMapSize
-	{
-		public double width;
-
-		public double height;
-	}
-
-	[StructLayout (LayoutKind.Sequential)]
-	public struct BMKMapRect
-	{
-		public BMKMapPoint origin;
-
-		public BMKMapSize size;
-	}*/
-
-	public enum BMKTransitStepType : uint
-	{
-		Busline = 0,
-		Subway = 1,
-		Wakling = 2
-	}
-
-	public enum BMKTransitPolicy : uint
-	{
-		TimeFirst = 3,
-		TransferFirst = 4,
-		WalkFirst = 5,
-		NoSubway = 6
-	}
-
-	public enum BMKDrivingPolicy
-	{
-		BlkFirst = -1,
-		TimeFirst = 0,
-		DisFirst = 1,
-		FeeFirst
-	}
-
-	public enum BMKDrivingRequestTrafficType : uint
-	{
-		None = 0,
-		PathAndTraffice = 1
-	}
-
-	public enum BMKPoiSortType : uint
-	{
-		Composite = 0,
-		Distance
-	}
-
-	public enum BMKRoutePlanShareURLType : uint
-	{
-		Drive = 0,
-		Walk = 1,
-		Ride = 2,
-		Transit = 3
-	}
+    public enum BMKRoutePlanShareURLType : uint
+    {
+        Drive = 0,
+        Walk = 1,
+        Ride = 2,
+        Transit = 3
+    }
 }
