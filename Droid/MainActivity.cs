3b48e65181f055cb36f067db2e30f63f1d711254
hug3id <hug3id@gmail.com>
2016-12-02 09:10:03 +0800

    Modify app name

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace Xamarin.Forms.BaiduMaps.Sample.Droid
 {
-    [Activity(Label = "Xamarin.Forms.BaiduMaps.Sample.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "BaiduMaps Sample", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
